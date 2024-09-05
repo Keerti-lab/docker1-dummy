49eca34a96b7b72e2d696658f9741d8f79d71a53
keerti-lab <keerti.gdev@gmail.com>
2024-09-05 16:57:44 +0530

    functions

--- mysql/scripts/schema.sql
+++ mysql/scripts/schema.sql
@@ -1,12 +1,12 @@
-CREATE DATABASE IF NOT EXISTS transactions
-use transactions;
+CREATE DATABASE IF NOT EXISTS transactions;
+USE transactions;
 
-CREATE TABLE IF NOT EXISTS transactions {
+CREATE TABLE IF NOT EXISTS transactions (
     id INT AUTO_INCREMENT PRIMARY KEY,
     amount INT,
     description VARCHAR(255)
-};
+);
 
-CREATE USER IF NOT EXISTS 'expense'@'%' IDENTIFIED BY 'Expenseapp@1'
-GRANT ALL ON transactions. * TO 'expense'@'%';
+CREATE USER IF NOT EXISTS 'expense'@'%' IDENTIFIED BY 'ExpenseApp@1';
+GRANT ALL ON transactions.* TO 'expense'@'%';
 FLUSH PRIVILEGES;
